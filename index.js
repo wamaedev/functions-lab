17496952c3d0c3a9d6a893c992190f113f67b9b2
Presbury Wamae <wamaedev@gmail.com>
2025-12-12 17:17:35 +0300

    Solved problem one

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blockNumber){
-  const numberOfBlocks = Math.abs(blockNumber - 42)
-  return numberOfBlocks
+  return Math.abs(blockNumber - 42);
 }
-console.log(distanceFromHqInBlocks(30))
+console.log(distanceFromHqInBlocks(30));
+
+function distanceFromHqInFeet(pickUpLocation){
+  const block = distanceFromHqInBlocks(pickUpLocation);
+  const feet = block * 264;
+  return feet;
+}
+console.log(distanceFromHqInFeet(30))
+
+function distanceTravelledInFeet(start, finish){
+  return (Math.abs(finish - start) * 264);
+}
+console.log(distanceTravelledInFeet(65, 60));
