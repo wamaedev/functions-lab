2d1476c758e37dd6a46f589e07db8bb5b2dc7f22
Presbury Wamae <wamaedev@gmail.com>
2025-12-17 12:41:30 +0300

    created the calculatesFarePrice function and its logic to fit the tests description

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function distanceFromHqInBlocks(blockNumber){
 }
 console.log(distanceFromHqInBlocks(30));
 
+
+
 function distanceFromHqInFeet(pickUpLocation){
   const block = distanceFromHqInBlocks(pickUpLocation);
   const feet = block * 264;
@@ -11,7 +13,25 @@ function distanceFromHqInFeet(pickUpLocation){
 }
 console.log(distanceFromHqInFeet(30))
 
+
+
 function distanceTravelledInFeet(start, destination){
   return (Math.abs(destination - start) * 264);
 }
 console.log(distanceTravelledInFeet(65, 60));
+
+
+
+function calculatesFarePrice(startBlock, destinationBlock){
+  let distance = distanceTravelledInFeet(startBlock, destinationBlock);
+  if(distance <= 400){
+    return 0
+  }else if(distance > 400 && distance <= 2000){
+    return (distance - 400) * 0.02
+  }else if (distance > 2000 && distance < 2500){
+    return 25
+  }else{
+    return "cannot travel that far"
+  }
+}
+console.log(calculatesFarePrice(34, 38))
